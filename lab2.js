70b228be89372c2bb7ca3e34728ca0e25a64a931
akom_work <akom07@gmail.com>
2024-05-22 18:19:33 +0300

    lab2

---
+++ lab2.js
@@ -0,0 +1,18 @@
+const sum =(num1 , num2)=>{
+    return num1+num2
+}
+const sub = (num1, num2)=>{
+    return num1 - num2
+}
+function divied(num1,num2){
+    return num1 / num2
+}
+const mult = function(num1,num2){
+    return num1*num2
+}
+
+
+let number1 = 100
+let number2 = 2
+
+console.log(sum(number1,number2) , sub(number1,number2) , divied(number1,number2) , mult(number1,number2))
